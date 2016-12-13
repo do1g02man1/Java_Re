93021c1d4bc4497883a8053ef1a8ead16a1eac36
Your Name <you@example.com>
2016-12-13 17:07:21 +0900

    f

---
+++ Ex02/src/LunarYear.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LunarYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		if(year % 4 == 0){
+			if(year % 100 == 0){
+				System.out.println(year + "년은 평년입니다");
+			} else {
+				System.out.println(year + "년은 윤년입니다");
+				 System.exit(year); 
+			}
+			
+			if(year % 400 == 0) {
+				System.out.println(year + "년은 융년입니다");
+				System.exit(year); 
+			} else {
+				System.out.println(year + "년은 윤년입니다");
+				System.exit(year); 
+			}
+		} else{
+			System.out.println(year + "년은 평년입니다");
+		}
+		sc.close();
+	}
+
+}
